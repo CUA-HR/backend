5417cec70a5d00e22e197150c07dbeebfd706208
Aymen Hadouara <aymenhadouara@gmail.com>
2024-09-30 14:49:48 +0100

    project intialization

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,12 +1,10 @@
-import { bigint, mysqlTable, timestamp, varchar } from "drizzle-orm/mysql-core";
+import { users } from "user/schema";
 
 
-export const users = mysqlTable("users", {
-    id: bigint("id", { mode: "number", unsigned: true })
-        .autoincrement()
-        .primaryKey(),
-    name: varchar("name", { length: 256 }).notNull(),
-    email: varchar("email", { length: 256 }).notNull().unique(),
-    createdAt: timestamp("created_at").defaultNow().notNull(),
-    updatedAt: timestamp("updated_at").defaultNow().onUpdateNow().notNull(),
-});
+
+
+
+module.exports = { 
+    users 
+
+}
---
+++ src/tier/schema.ts
@@ -0,0 +1,13 @@
+import { bigint, mysqlTable, timestamp, varchar } from "drizzle-orm/mysql-core";
+
+
+export const users = mysqlTable("users", {
+    id: bigint("id", { mode: "number", unsigned: true })
+        .autoincrement()
+        .primaryKey(),
+    name: varchar("name", { length: 256 }).notNull(),
+    email: varchar("email", { length: 256 }).notNull().unique(),
+    password: varchar("password", { length: 256 }).notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().onUpdateNow().notNull(),
+});
